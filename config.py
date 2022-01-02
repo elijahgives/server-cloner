19414d83cd696a9e2a742998fa037f6473bff582
ElijahGives <elijahbocook@gmail.com>
2022-01-02 09:31:26 -0600

    fix(variables): fix cloner variables, add windows option

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ input_guild_id = "901516873294831706"  # The ID of the server that you're copyin
 output_guild_id = "906298110299484182"  # The ID of the server that you're pasting onto
 token = "ACCOUNT_TOKEN"  # <-- Account token. (user)
 clear_server = True # Whether to clear the server you're pasting to or not (To delete old channels, roles, etc)
+windows = True # Whether you're on windows or not (for titling)
--- main.py
+++ main.py
@@ -6,23 +6,25 @@ from config import *
 init(autoreset=True, convert=True)
 
 client = discord.Client()
-
-os.system("title ServerCloner - Starting...")
+if windows:
+    os.system("title ServerCloner - Starting...")
 print("ServerCloner - Starting...\n")
 
 from src.cloner import ServerCloner
             
-async def start(client, input_guild, output_guild):
+async def start():
     guild = client.get_guild(int(input_guild_id))
     new_guild = client.get_guild(int(output_guild_id))
 
-    cloner = ServerCloner(client, input_guild, output_guild, clear=clear_server)
+    cloner = ServerCloner(client, guild, new_guild, clear=clear_server)
     await cloner.start()
 
 async def finish():
-    os.system('title ServerCloner - Completed')
+    if windows:
+        os.system('title ServerCloner - Completed')
     log(blue+'[ServerCloner]'+r, 'Server cloning process completed.')
-    os.system('pause')
+    if windows:
+        os.system('pause')
 
 @client.event
 async def on_ready():
